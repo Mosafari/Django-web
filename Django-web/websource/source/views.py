8540cfc07edff7f0ddcace79211e585a167b8695
mosafari <m.safari2680@gmail.com>
2022-11-27 21:58:20 +0330

    add another page & make it reachable

--- Django-web/websource/source/views.py
+++ Django-web/websource/source/views.py
@@ -5,3 +5,7 @@ from django.template import loader
 def index(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
+
+def reso(request):
+    template = loader.get_template('resources.html')
+    return HttpResponse(template.render())
